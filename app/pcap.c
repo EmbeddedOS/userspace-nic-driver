fbc4d0e3c2e9e47a55ab4e6d85843d64a1aad889
cong.nguyenthanh <cong.nguyenthanh@vvdntech.com>
2025-05-31 08:33:10 -0800

    mempool creating

--- app/pcap.c
+++ app/pcap.c
@@ -1,7 +1,22 @@
 #include <stdio.h>
+#include <fcntl.h>
 
+#include <utility.h>
 #include <log.h>
 #include <driver/base.h>
+
+static int mmap_2mb_page(int32_t length, uint8_t **addr)
+{
+    int res = 0;
+    int fd = 0;
+    char temp_huge_file[40] = {0};
+
+    generate_unique_filepath(temp_huge_file, sizeof(temp_huge_file));
+    log_info("Generated huge backed file: %s", temp_huge_file);
+
+
+    res = open(temp_huge_file, O_CREAT | O_EXCL | O_RDWR, 0777);
+}
 
 int main()
 {
@@ -9,4 +24,7 @@ int main()
 
     struct nic_driver *self = nic_driver_init("0000:00:01.0");
     self->send(self, NULL, 0);
+
+    mmap_2mb_page(100, NULL);
 }
+
--- util/mempool.c
+++ util/mempool.c
@@ -1,8 +1,20 @@
 #include <sys/mman.h>
 #include <stddef.h>
+#include <fcntl.h>
+#include <stdlib.h>
 
 #include <log.h>
+#include <utility.h>
 #include <mempool.h>
+
+static char *generate_unique_filepath(char *buf, int len)
+{
+    char random_filename[10] = {0};
+
+    snprintf(buf, len, "%s/%s", HUGE_PAGE_MOUNT_POINT,
+             generate_random_string(random_filename, sizeof(random_filename)));
+    return buf;
+}
 
 static inline int vfio_mmap_2mb_page(int32_t length, uint8_t **addr)
 {
@@ -24,9 +36,14 @@ static int mmap_2mb_page(int32_t length, uint8_t **addr)
 {
     int res = 0;
     int fd = 0;
-    res = open();
-}
+    char temp_huge_file[40] = {0};
 
+    generate_unique_filepath(temp_huge_file, sizeof(temp_huge_file));
+    log_info("Generated huge backed file: %s", temp_huge_file);
+
+
+    res = open(temp_huge_file, O_CREAT | O_EXCL | O_RDWR, 0777);
+}
 
 int mempool_create(uint32_t length)
 {
