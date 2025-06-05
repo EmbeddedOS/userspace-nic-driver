b12c87a36b637de170f2df77f4c0089081446eab
EmbeddedOS <nguyenthanhcong12gtm1@gmail.com>
2025-06-05 01:30:13 -0800

    wip

--- driver/base.c
+++ driver/base.c
@@ -4,6 +4,12 @@
 #include <pci.h>
 #include <e1000e.h>
 
+/**
+ * @brief   - Initialize a nic driver on userspace with pci address.
+ * @note    - Every nic device is connected to PCI bus, by using the device PCI
+ *            address, we get the device information and load the corresponding
+ *            driver initializer.
+ */
 struct nic_driver *nic_driver_init(const char *pci_addr)
 {
     struct pci_dev_info pci_info = {0};
@@ -12,11 +18,12 @@ struct nic_driver *nic_driver_init(const char *pci_addr)
     res = pci_get_info(pci_addr, &pci_info);
     expr_check_err(res, exit, "pci_get_info failed");
 
-    if (pci_info.device_id == INTEL_82574L_GIGABIT_DEVICE_ID && 
+    if (pci_info.device_id == INTEL_82574L_GIGABIT_DEVICE_ID &&
         pci_info.vendor_id == INTEL_CORP_VENDOR_ID)
     {
         return e1000e_init(pci_addr);
     }
+    // Adding more device driver init here.
     else
     {
         log_error("Can't find driver!");
