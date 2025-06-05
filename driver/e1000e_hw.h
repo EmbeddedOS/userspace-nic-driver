89e1d034c12dda8e8ef13c021cda12458c798d65
EmbeddedOS <cong.nguyenthanh@vvdntech.com>
2025-06-05 01:22:46 -0800

    add descriptor structure

--- driver/e1000e_hw.h
+++ driver/e1000e_hw.h
@@ -30,13 +30,39 @@
 #define INTEL_82574_RXERRC_OFFSET 0x0400C
 #define INTEL_82574_MPC_OFFSET 0x04010
 
+#define INTEL_82574_PRC64_OFFSET 0x0405C /* Packets received 64 bytes count. */
+#define INTEL_82574_PRC127_OFFSET 0x04060
+#define INTEL_82574_PRC255_OFFSET 0x04064
+#define INTEL_82574_PRC511_OFFSET 0x04068
+#define INTEL_82574_PRC1023_OFFSET 0x0406C
+#define INTEL_82574_PRC1522_OFFSET 0x04070
+
+#define INTEL_82574_GPRC_OFFSET 0x04074 /* Good packet received count. */
+#define INTEL_82574_BPRC_OFFSET 0x04078
+#define INTEL_82574_MPRC_OFFSET 0x0407C
+#define INTEL_82574_GPTC_OFFSET 0x04080 /* Good packet transmitted count. */
+#define INTEL_82574_GORCL_OFFSET 0x04088
+#define INTEL_82574_GORCH_OFFSET 0x0408C
+#define INTEL_82574_GOTCL_OFFSET 0x04090
+#define INTEL_82574_GOTCH_OFFSET 0x04094
+#define INTEL_82574_TORL_OFFSET 0x040C0 /* Total Octets Received Low. */
+#define INTEL_82574_TORH_OFFSET 0x040C4 /* Total Octets Received High. */
+#define INTEL_82574_TPR_OFFSET 0x040D0  /* Total packets received. */
+#define INTEL_82574_TPT_OFFSET 0x040D4  /* Total packets transmitted. */
+
+#define INTEL_82574_PTC64_OFFSET 0x040D8 /* Packets transmitted bytes count. */
+#define INTEL_82574_PTC127_OFFSET 0x040DC
+#define INTEL_82574_PTC255_OFFSET 0x040E0
+#define INTEL_82574_PTC511_OFFSET 0x040E4
+#define INTEL_82574_PTC1023_OFFSET 0x040E8
+#define INTEL_82574_PTC1522_OFFSET 0x040EC
+
 /* Intel 82574 PHY register offsets ------------------------------------------*/
 #define INTEL_82574_PHY_CTRL_OFFSET 0x00
 #define INTEL_82574_PHY_STATUS_OFFSET 0x01
 #define INTEL_82574_PHY_IDENTIFIER1_OFFSET 0x02
 #define INTEL_82574_PHY_IDENTIFIER2_OFFSET 0x03
 #define INTEL_82574_PHY_EXTENDED_STATUS_OFFSET 0x0f
-
 
 /* Intel 82574 register bits -------------------------------------------------*/
 /* Interrupt Mask Set/Read bits. */
@@ -56,7 +82,6 @@
 #define INTEL_82574_MDIC_I_BIT 0x1D
 #define INTEL_82574_MDIC_E_BIT 0x1E
 
-
 /* Intel 82574 register masks ------------------------------------------------*/
 #define INTEL_82574_CTRL0_RST_MASK (1 << INTEL_82574_CTRL0_RST_BIT)
 #define INTEL_82574_CTRL0_PHY_RST_MASK (1 << INTEL_82574_CTRL0_PHY_RST_BIT)
@@ -67,3 +92,28 @@
 
 #define INTEL_82574_MDIC_OP_MDI_WRITE 0b01
 #define INTEL_82574_MDIC_OP_MDI_READ 0b10
+
+/* Intel 82574 fields --------------------------------------------------------*/
+
+#pragma pack(push, 1)
+struct e1000e_legacy_rx_desc
+{ // Legacy Receive Descriptor Format.
+    uint64_t addr;
+    uint16_t len;
+    uint16_t checksum;
+    uint8_t status;
+    uint8_t error;
+    uint16_t vlan_tags;
+};
+
+struct e1000e_extened_rx_desc
+{ // Extended Rx Descriptor.
+    uint64_t addr;
+    uint16_t len;
+    uint16_t checksum;
+    uint8_t status;
+    uint8_t error;
+    uint16_t vlan_tags;
+};
+
+#pragma pack(pop)
